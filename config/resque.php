22193a7a5b6ecae1064958d0d45d5f2ce2189e40
Mike Smullin <mike@smullindesign.com>
2011-07-29 16:57:30 -0600

    customize resque config

--- config/resque.php
+++ config/resque.php
@@ -1,19 +1,19 @@
 <?php
 
-switch (Configure::read('YourApp.environment')) {
-  default:
-  case 'production':
-    $config['Resque']['Redis'] = array(
-      'host' => 'localhost', // replace with outside server for best performance
-      'port' => 6379
-    );
-    break;
+switch (Configure::write('Qoggo.environment', $env)) {
+	default:
+	case 'production':
+		$config['Resque']['Redis'] = array(
+			'host' => 'localhost', // replace with outside server for best performance
+			'port' => 6379
+		);
+		break;
 
-  case 'staging':
-  case 'local':
-    $config['Resque']['Redis'] = array(
-      'host' => 'localhost',
-      'port' => 6379
-    );
-    break;
+	case 'staging':
+	case 'local':
+		$config['Resque']['Redis'] = array(
+			'host' => 'localhost',
+			'port' => 6379
+		);
+		break;
 }
